ca5cd7085f0386b78682fac1477372e473e65701
arimax <arimax04@gmail.com>
2023-03-26 19:25:35 +0900

    implement 102

---
+++ Assets/Fusion102/Scripts/NetworkInputData.cs
@@ -0,0 +1,13 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FusionTrial.Fusion102
+{
+    public struct NetworkInputData : INetworkInput
+    {
+        public Vector3 direction;
+    }
+}
+
---
+++ Assets/Fusion102/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using Fusion;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FusionTrial.Fusion102
+{
+    public class Player : NetworkBehaviour
+    {
+        private NetworkCharacterControllerPrototype _cc;
+
+        private void Awake()
+        {
+            _cc = GetComponent<NetworkCharacterControllerPrototype>();
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (GetInput(out NetworkInputData data))
+            {
+                data.direction.Normalize();
+                _cc.Move(5 * data.direction * Runner.DeltaTime);
+            }
+        }
+    }
+}
